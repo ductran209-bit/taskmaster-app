e1da4fba427244535bfbfa1b73dc8e6471bd1f1d
ductran209-bit <email_cua_ban@gmail.com>
2026-07-30 10:43:10 +0700

    Update valid Firebase API key

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtk3nosFwnVVhJVHQuAKzJaDScd3LgJUw",
+  apiKey: "AIzaSyCtk3nosFWnVVhJVHQuAKzJaDScd3LgJUw",
   authDomain: "taskmaster-app-102f3.firebaseapp.com",
   projectId: "taskmaster-app-102f3",
   storageBucket: "taskmaster-app-102f3.firebasestorage.app",
@@ -12,10 +12,7 @@ const firebaseConfig = {
   measurementId: "G-B9VQBR3PDV"
 };
 
-// Khởi tạo Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export auth và db để App.jsx có thể sử dụng
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
